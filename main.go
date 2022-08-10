8414f7cacec363071e6009c19e65d488f5e9567b
Guillaume <guillaume.dupuy@ynov.com>
2022-08-10 12:24:57 +0200

    add cors site

--- main.go
+++ main.go
@@ -145,6 +145,8 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
+	r.Use(CORSMiddleware())
+
 	r.GET("/", home)
 
 	r.GET("/userPYC", getAllUsersPYC)
@@ -174,6 +176,22 @@ func main() {
 
 	fmt.Println("\n" + "Voici le lien du serveur :" + " http://localhost:8080/")
 	r.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
 
 // route functions
