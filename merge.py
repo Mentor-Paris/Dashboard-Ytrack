26cf8645b9a1658056450cedb5f2708d723c934b
Guillaume <guillaume.dupuy@ynov.com>
2022-09-14 00:06:55 +0200

    fix leaderboard national

--- merge.py
+++ merge.py
@@ -6,7 +6,7 @@ cwd = os.getcwd()
 usernational = pd.read_json(cwd+"/assets/json/usersnational.json")
 usercampus = pd.read_json(cwd+"/assets/json/usersnationalxp.json")
 
-usercampus = usercampus.rename(columns={'firstName': 'login'})
+usercampus = usercampus.rename(columns={'login': 'login'})
 
 merged = usernational.merge(usercampus, on='login', how="outer")
-merged.to_csv(cwd+"/assets/files/merge_data_user2.csv", index=False)
+merged.to_csv(cwd+"/assets/files/merge_data_user.csv", index=False)
