ed787ea31d7b177669f87643204a2c2e59ccc712
Guillaume <guillaume.dupuy@ynov.com>
2023-07-04 17:10:29 +0200

    update code graph

--- merge.py
+++ merge.py
@@ -9,4 +9,13 @@ usercampus = pd.read_json(cwd+"/assets/json/usersnationalxp.json")
 usercampus = usercampus.rename(columns={'login': 'login'})
 
 merged = usernational.merge(usercampus, on='login', how="outer")
+
+merged["xps_aggregate"] = merged["xps_aggregate"].fillna(0)
+
+for i in range (0, len(merged)):
+    if merged.xps_aggregate[i] :
+        merged.xps_aggregate[i] = merged.xps_aggregate[i]['aggregate']['sum']['amount']
+    else :
+        merged.xps_aggregate[i] = 0
+
 merged.to_csv(cwd+"/assets/files/merge_data_user.csv", index=False)
